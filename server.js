bc319dc38801a082cbac04335b21aab81e576afe
Megan Coppins <meganlcoppins@gmail.com>
2020-03-05 10:33:38 -0700

    read and return saved notes commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const db = require("./db/db.json");
+const db = require("./db/db.js");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true}));
@@ -26,6 +26,18 @@ app.get("/api/notes", function(req, res){
     return res.json(db);
 })
 // POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+
+// app.post("/api/notes", function(req, res) {
+//     const notes = req.body;
+
+//     let myObj = {
+//         id = db.length,
+//         title: notes.title,
+//         text: notes.text
+//     }
+//     db.push(myObj);
+//     console.log(db.json);
+// })
 
 // DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 
