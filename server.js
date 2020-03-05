dd85920f7794d9e75168416341a0f56278065047
Megan Coppins <meganlcoppins@gmail.com>
2020-03-04 20:24:59 -0700

    html routes commit

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const db = require("./db/db.json");
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
@@ -18,6 +20,14 @@ app.use(express.static("public"));
 //     res.sendFile(__dirname + "/public/assets/js/index.js");
 // });
 
+
+// GET /api/notes - Should read the db.json file and return all saved notes as JSON.
+app.get("/api/notes", function(req, res){
+    return res.json(db);
+})
+// POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+
+// DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 
 // GET /notes - Should return the notes.html file. (HTML route)
 app.get("/notes", function(req, res){
